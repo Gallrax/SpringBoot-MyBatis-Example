badb5edee4bfa85900029d8525bd7c63db7f4050
Gallrax <827269727@qq.com>
2017-07-10 17:57:24 +0800

    -

--- src/main/java/com/sm/Application.java
+++ src/main/java/com/sm/Application.java
@@ -4,7 +4,14 @@ import org.apache.log4j.Logger;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 /**
  * Created by Gallrax on 2017/7/5.
@@ -20,5 +27,29 @@ public class Application {
         logger.info("----- 启动中 -----");
         SpringApplication.run(Application.class, args);
         logger.info("----- 启动成功 -----");
+    }
+
+    //配置文件两种方式，一种内部类(内部类需为静态内部类，配置文件理应优先加载)，另外一种单独写一个公开类
+    @Configuration
+    static class LoginIntercetor extends WebMvcConfigurerAdapter {
+        @Override
+        public void addInterceptors(InterceptorRegistry registry) {
+            registry.addInterceptor(new HandlerInterceptorAdapter() {
+                @Override
+                public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+
+                    logger.info("----- 此次访问的URI："+ request.getRequestURI() +" -----");
+                    logger.info("----- 正在验证登录状态");
+                    String username = (String) request.getSession().getAttribute("username");
+                    if(username == null || username.isEmpty()){
+                        logger.info("----- 用户未登录 -----");
+                        response.sendRedirect("login");
+                        return false;
+                    }
+                    logger.info("----- 用户已登录 -----");
+                    return true;
+                }
+            }).addPathPatterns("/**").excludePathPatterns("/login*");//不会拦截位于static的文件
+        }
     }
 }
--- src/main/java/com/sm/controller/IndexController.java
+++ src/main/java/com/sm/controller/IndexController.java
@@ -2,6 +2,9 @@ package com.sm.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.servlet.http.HttpSession;
 
 /**
  * Created by Gallrax on 2017/7/5.
@@ -14,9 +17,29 @@ public class IndexController {
         throw new RuntimeException("----- this is Exception -----");
     }
 
-    @RequestMapping("/index.html")
+    /*@RequestMapping("/index.html")
     public String index() {
         return "index";
+    }*/
+
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
+    public String login() {
+        return "login";
+    }
+
+    @RequestMapping(value = "/loginValidate", method = RequestMethod.POST)
+    public String validate(String username, String password, HttpSession session) {
+        if("admin".equals(username) && "admin".equals(password)){
+            session.setAttribute("username", username);
+            return "index";
+        }
+        return "login";
+    }
+
+    @RequestMapping(value = "/logout", method = RequestMethod.GET)
+    public String logout(HttpSession session) {
+        session.removeAttribute("username");
+        return "login";
     }
 
 }
